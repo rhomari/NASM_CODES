7b460f4ca839a213efeb45afe61b9f7729407edc
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-08-10 22:52:04 +0100

    .

--- Extern C/main.c
+++ Extern C/main.c
@@ -1,9 +1,12 @@
+#include <windows.h>
+
 int main()
 {
-    extern int PrintColoredText(char*, int);
+    extern int PrintColoredText(char*, unsigned short);
     char * text = "ASM is fun...";
     PrintColoredText(text, 5);
     return 0;
+    
 }
 // nasm -f win64 -o function.obj function.asm
 // gcc function.obj main.c -o main.exe
